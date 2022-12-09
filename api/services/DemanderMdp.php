2b4f01bc74e7aee65efed8d7ab0b602658035c6f
delasalle-sio-kergoat-m <delasalle.sio.kergoat.m@gmail.com>
2022-12-09 09:56:31 +0100

    DemanderMdp.php fini et fonctionnel

--- api/services/DemanderMdp.php
+++ api/services/DemanderMdp.php
@@ -8,32 +8,41 @@ $dao = new DAO();
 // Récupération des données transmises
 $pseudo = ( empty($this->request['pseudo'])) ? "" : $this->request['pseudo'];
 $lang = ( empty($this->request['lang'])) ? "" : $this->request['lang'];
+$nouveauMdp = ( empty($this->request['nouveauMdp'])) ? "" : $this->request['nouveauMdp'];
 
-if ($this->getMethodeRequete() != "GET")
-{	$msg = "Erreur : méthode HTTP incorrecte.";
-$code_reponse = 406;
-}
-else
+if ($lang != "json") $lang = "xml";
+
+// Les paramètres doivent être présents
+if ( $pseudo == "" || $lang == "") 
 {
-    // Les paramètres doivent être présents
-    if ( $pseudo == "" || $mdpSha1 == "" || $nouveauMdp == "" || $confirmationMdp == "" )
+    $msg = "Erreur : données incomplètes.";
+    $code_reponse = 400;
+}
+else 
+{
+    if (!($dao->existePseudoUtilisateur($pseudo))) 
     {
-        $msg = "Erreur : données incomplètes.";
+        $msg = 'Erreur : pseudo inexistant.';
         $code_reponse = 400;
     }
-    else
+    else 
     {
-        if ( strlen($nouveauMdp) < 3 )
+        $ok = $dao->envoyerMdp ($pseudo, $nouveauMdp);
+        if ( $ok ) 
         {
-            $msg = 'Erreur : le mot de passe doit comporter au moins 8 caractères.';
-            $code_reponse = 400;
+            $msg = "Vous allez recevoir un courriel de confirmation avec votre nouveau mot de passe.";
+            $code_reponse = 200;
         }
-        else
+        else 
         {
-            
+            $msg = "Enregistrement effectué ; l'envoi du courriel de confirmation a rencontré un problème.";
+            $code_reponse = 500;              
         }
     }
 }
+
+// ferme la connexion à MySQL :
+unset($dao);
 
 // création du flux en sortie
 if ($lang == "xml") {
@@ -51,9 +60,19 @@ $this->envoyerReponse($code_reponse, $content_type, $donnees);
 // fin du programme (pour ne pas enchainer sur les 2 fonctions qui suivent)
 exit;
 
+// ================================================================================================
 
+// création du flux XML en sortie
 function creerFluxXML($msg)
 {
+    /* Exemple de code XML
+     <?xml version="1.0" encoding="UTF-8"?>
+     <!--Service web ChangerDeMdp - BTS SIO - Lycée De La Salle - Rennes-->
+     <data>
+     <reponse>Erreur : authentification incorrecte.</reponse>
+     </data>
+     */
+    
     // crée une instance de DOMdocument (DOM : Document Object Model)
     $doc = new DOMDocument();
     
@@ -62,7 +81,7 @@ function creerFluxXML($msg)
     $doc->encoding = 'UTF-8';
     
     // crée un commentaire et l'encode en UTF-8
-    $elt_commentaire = $doc->createComment('Service web DemanderMdp - BTS SIO - Lycée De La Salle - Rennes');
+    $elt_commentaire = $doc->createComment('Service web ChangerDeMdp - BTS SIO - Lycée De La Salle - Rennes');
     // place ce commentaire à la racine du document XML
     $doc->appendChild($elt_commentaire);
     
@@ -81,16 +100,28 @@ function creerFluxXML($msg)
     return $doc->saveXML();
 }
 
+// ================================================================================================
+
 // création du flux JSON en sortie
 function creerFluxJSON($msg)
 {
+    /* Exemple de code JSON
+     {
+     "data": {
+     "reponse": "Erreur : authentification incorrecte."
+     }
+     }
+     */
+    
+    // construction de l'élément "data"
     $elt_data = ["reponse" => $msg];
-        
+    
     // construction de la racine
     $elt_racine = ["data" => $elt_data];
-        
+    
     // retourne le contenu JSON (l'option JSON_PRETTY_PRINT gère les sauts de ligne et l'indentation)
     return json_encode($elt_racine, JSON_PRETTY_PRINT);
 }
 
+// ================================================================================================
 ?>
